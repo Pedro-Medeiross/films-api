fee7f11b7b16cf0dad5b15d407e64cd1b07858fc
Pedro Medeiros <pedro.contnato166@hotmail.com>
2022-12-27 10:24:19 -0300

    Initial commit

---
+++ app/Http/Controllers/FilmsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\FilmsRequest;
+use App\Models\Films;
+use Illuminate\Http\Request;
+
+class FilmsController extends Controller
+{
+    public function index()
+    {
+        $films = Films::all();
+        return json_encode($films, 200);
+    }
+
+    public function store(FilmsRequest $request)
+    {
+        $film = Films::create($request->all());
+        return json_encode($film, 201);
+    }
+
+    public function show(Films $film)
+    {
+        return json_encode($film, 200);
+    }
+
+    public function update(FilmsRequest $request, Films $film)
+    {
+        $film->update($request->all());
+        return json_encode($film, 200);
+    }
+
+    public function destroy(Films $film)
+    {
+        $film->delete();
+        return json_encode(null, 204);
+    }
+}
